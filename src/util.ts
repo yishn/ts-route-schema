381e0a77fbf1df9cfcab2ea73142e6d9952c1bc3
Yichuan Shen <shen.yichuan@gmail.com>
2020-07-19 02:20:29 +0200

    Add isContentType()

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,6 @@
+import type {Request, Params, Query} from 'express-serve-static-core'
+import type {ContentType} from './types'
+
 declare const phantom: unique symbol
 
 export interface Declare<T> {
@@ -5,3 +8,11 @@ export interface Declare<T> {
 }
 
 export type Wrap<T, U> = T & Declare<U>
+
+export function isContentType<Pm extends Params, R, B, Q extends Query, C extends string>(
+  req: Request<Pm, R, B, Q>,
+  body: B,
+  contentType: C
+): body is Extract<B, ContentType<C>> {
+  return req.get('Content-Type') === contentType
+}
