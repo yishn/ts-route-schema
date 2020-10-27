831b8cb2fb1869afec12e8659b73e67f57d69c73
Yichuan Shen <shen.yichuan@gmail.com>
2020-10-27 13:25:08 +0100

    Fix tests

--- test/types.test.ts
+++ test/types.test.ts
@@ -1,22 +1,8 @@
 import { expectType, TypeEqual } from 'ts-expect'
 import * as t from 'tap'
-import { IsAny, IsUnknown, RequestData, ResponseData } from '../src/main'
+import { RequestData, ResponseData } from '../src/main'
 
 t.test('types', async t => {
-  t.test('IsAny', async t => {
-    expectType<IsAny<any>>(true)
-    expectType<IsAny<unknown>>(false)
-    expectType<IsAny<5>>(false)
-    expectType<IsAny<'str'>>(false)
-  })
-
-  t.test('IsUnknown', async t => {
-    expectType<IsUnknown<any>>(false)
-    expectType<IsUnknown<unknown>>(true)
-    expectType<IsUnknown<5>>(false)
-    expectType<IsUnknown<'str'>>(false)
-  })
-
   t.test('RequestData', async t => {
     expectType<
       TypeEqual<
